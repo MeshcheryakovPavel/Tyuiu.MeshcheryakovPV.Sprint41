5582199b8a7f31173b7c9927dddb7ab07768a386
Павел Мещеряков <pavellol20@gmail.com>
2023-11-05 21:11:01 +0500

    Task1

---
+++ Tyuiu.MeshcheryakovPV.Sprint4.Task1.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MeshcheryakovPV.Sprint4.Task1.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MeshcheryakovPV.Sprint4.Task1.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("693910f7-43c8-43c8-b32d-f850d6485f97")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
